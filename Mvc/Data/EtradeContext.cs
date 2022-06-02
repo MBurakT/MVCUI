5f1dea77166521c22dea4163065a3843db679069
MBurakT <mbtbrk11@outlook.com>
2022-06-02 17:12:12 +0300

    .

---
+++ Mvc/Data/EtradeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mvc.Data
+{
+    public class EtradeContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=ETRADE4;Integrated Security=True");
+        }
+    }
+}
